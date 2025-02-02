f3aab7fb68c8b3155b871127961859e480850d6d
Néstor Nicolás Campos Rojas <nescampos_18@hotmail.com>
2025-02-02 16:10:44 -0300

    first commit

---
+++ src/viem/createViemPublicClient.ts
@@ -0,0 +1,28 @@
+import { createPublicClient, http } from "viem";
+import { flareTestnet } from "viem/chains";
+
+/**
+ * Creates a new public Viem client connected to the Flare testnet.
+ *
+ * A public client is a client that is not connected to a specific wallet and
+ * therefore cannot perform write operations.
+ *
+ * @returns A new public Viem client.
+ */
+export function createViemPublicClient() {
+  return createPublicClient({
+    /**
+     * The chain to connect to.
+     *
+     * In this case, we're using the Flare testnet.
+     */
+    chain: flareTestnet,
+
+    /**
+     * The transport to use for HTTP requests.
+     *
+     * In this case, we're using the built-in HTTP transport.
+     */
+    transport: http(),
+  });
+}
